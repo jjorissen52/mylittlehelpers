58c58cb2003ca9ab7aaf50881a91ce0a6db220e1
John-Paul <jjorissen@americanphysician.partners>
2017-08-28 09:30:44 -0500

    added weak file encryption

--- mylittlehelpers.py
+++ mylittlehelpers.py
@@ -3,6 +3,10 @@ import json
 import os
 import time
 import smtplib
+from os import urandom
+from hashlib import md5
+
+from Crypto.Cipher import AES
 from math import trunc
 
 import sys
@@ -170,7 +174,6 @@ class InterfaceSettings:
 
 
 def send_gmail(recipient, subject, body, user=None, pwd=None):
-
     gmail_user = user
     gmail_pwd = pwd
     FROM = user
@@ -205,7 +208,7 @@ def time_check(start):
     return start, auto_refresh
 
 
-def print_duration(start):
+def time_elapsed(start):
     current_time = time.time()
     hours, rem = divmod(current_time - start, 3600)
     minutes, seconds = divmod(rem, 60)
@@ -227,3 +230,52 @@ def time_bomb(countdown, package=(print, ("BOOM",)), action="", dots=3):
         sys.stdout.flush()
     print("")
     package[0](*package[1])
+
+
+def derive_key_and_iv(password, salt, key_length, iv_length):
+    d = d_i = b''  # changed '' to b''
+    while len(d) < key_length + iv_length:
+        # changed password to str.encode(password)
+        d_i = md5(d_i + str.encode(password) + salt).digest()
+        d += d_i
+    return d[:key_length], d[key_length:key_length+iv_length]
+
+
+def encrypt(in_file, out_file, password, salt_header='', key_length=32):
+    # added salt_header=''
+    bs = AES.block_size
+    # replaced Crypt.Random with os.urandom
+    salt = urandom(bs - len(salt_header))
+    key, iv = derive_key_and_iv(password, salt, key_length, bs)
+    cipher = AES.new(key, AES.MODE_CBC, iv)
+    # changed 'Salted__' to str.encode(salt_header)
+    out_file.write(str.encode(salt_header) + salt)
+    finished = False
+    while not finished:
+        chunk = in_file.read(1024 * bs)
+        if len(chunk) == 0 or len(chunk) % bs != 0:
+            padding_length = (bs - len(chunk) % bs) or bs
+            # changed right side to str.encode(...)
+            chunk += str.encode(
+                padding_length * chr(padding_length))
+            finished = True
+        out_file.write(cipher.encrypt(chunk))
+
+
+def decrypt(in_file, out_file, password, salt_header='', key_length=32):
+    # added salt_header=''
+    bs = AES.block_size
+    # changed 'Salted__' to salt_header
+    salt = in_file.read(bs)[len(salt_header):]
+    key, iv = derive_key_and_iv(password, salt, key_length, bs)
+    cipher = AES.new(key, AES.MODE_CBC, iv)
+    next_chunk = ''
+    finished = False
+    while not finished:
+        chunk, next_chunk = next_chunk, cipher.decrypt(
+            in_file.read(1024 * bs))
+        if len(next_chunk) == 0:
+            padding_length = chunk[-1]  # removed ord(...) as unnecessary
+            chunk = chunk[:-padding_length]
+            finished = True
+        out_file.write(bytes(x for x in chunk))  # changed chunk to bytes(...)
