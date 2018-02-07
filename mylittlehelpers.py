42677e8e46af7c51e2f5f7b935bb40b69fb55742
jp <jjorissen52@gmail.com>
2018-02-07 09:51:55 -0600

    fixed bug where send_email will not allow absence of attachment

--- mylittlehelpers.py
+++ mylittlehelpers.py
@@ -11,11 +11,19 @@ from io import BytesIO
 import ntpath
 import configparser
 
+
 class ImproperlyConfigured(BaseException):
     pass
 
 
 def __except__(exception, replacement_function):
+    """
+    Function wrapper. If the wrapped function encounters the specified exception, it will instead perform the replacement function (with the same arguments).
+    
+    :param exception: (Exception) 
+    :param replacement_function: 
+    :return: 
+    """
     def _try_wrap(function):
         def __try_wrap(*__args, **__kwargs):
             try:
@@ -39,6 +47,13 @@ def time_check(start):
 
 
 def time_elapsed(start, string_width=12):
+    """
+    Returns time elapsed in "HH:MM:SS" format.
+
+    :param start: (int, float) unix epoch
+    :param string_width: (int) width of returned string
+    :return: (str) time elapsed since start in "HH:MM:SS" format
+    """
     current_time = time.time()
     hours, rem = divmod(current_time - start, 3600)
     minutes, seconds = divmod(rem, 60)
@@ -47,6 +62,15 @@ def time_elapsed(start, string_width=12):
 
 
 def time_bomb(countdown, package=(print, ("BOOM",)), action="", dots=3):
+    """
+    tells the user that {action} will be done in {countdown} seconds. package[0] is the function and package[1] is package[0] is a list of arguments to be passed to package[0]
+
+    :param countdown: (int) time until package[0] is called
+    :param package: (list, tuple) package[0] is a function object, package[1] is a tuple or list of arguments to be passed to package[0]
+    :param action: (str) description of what the package will do
+    :param dots: (int) number of dots between counts
+    :return:
+    """
     action = action if action else package[0].__name__
     sys.stdout.write(f"{action} in {countdown}")
     sys.stdout.flush()
@@ -62,7 +86,14 @@ def time_bomb(countdown, package=(print, ("BOOM",)), action="", dots=3):
     package[0](*package[1])
 
 def setup_logger(name, log_file, level=logging.INFO):
-    """Function setup as many loggers as you want"""
+    """
+    sets up a file logger with log level INFO
+
+    :param name: (str) name of logger
+    :param log_file: (str) path to log file location
+    :param level: (logging.level)
+    :return: (logging.logger)
+    """
     try:
         os.makedirs(os.path.dirname(log_file))
     except FileExistsError:
@@ -77,6 +108,12 @@ def setup_logger(name, log_file, level=logging.INFO):
     return logger
 
 def remove_if_exists(file_name):
+    """
+    removes file located at specified path of it exists, otherwise does nothing.
+
+    :param file_name: (str) path to file
+    :return:
+    """
     try:
         os.remove(file_name)
     except OSError:
@@ -84,6 +121,16 @@ def remove_if_exists(file_name):
 
 
 def send_gmail(recipient, subject, body, user=None, pwd=None):
+    """
+    sends an email to the specified recipient(s)
+
+    :param recipient: (str, list) recipient email address(es)
+    :param subject: (str) subject line
+    :param body: (str) email body
+    :param user: (str) sender email address
+    :param pwd: (str) sender password
+    :return:
+    """
     gmail_user = user
     gmail_pwd = pwd
     FROM = user
@@ -111,11 +158,26 @@ class SendError(BaseException):
 
 
 def binary(file_path):
+    """
+    Read specified file as binary and return its name and the binary contents
+
+    :param file_path: (str) path to file
+    :return:
+    """
     with open(file_path, 'rb') as binary_chunk:
         return ntpath.basename(file_path), binary_chunk.read()
 
 
 def get_credentials(path, region="microsoft_exchange_email", address_key="email", password_key="email_password"):
+    """
+    return email address and password from specified region and keys
+
+    :param path: (str) path to config file
+    :param region: (str) region containing email credentials
+    :param address_key: (str) key containing email address in region
+    :param password_key: (str) key containing email password in region
+    :return: (dict) with keys email_address and email_password
+    """
     config = configparser.ConfigParser(interpolation=configparser.ExtendedInterpolation())
     config.read(path)
     credentials = {"email_address": config.get(region, address_key), "email_password": config.get(region, password_key)}
@@ -123,6 +185,17 @@ def get_credentials(path, region="microsoft_exchange_email", address_key="email"
 
 
 def send_email(credentials, email_list=None, email_subject='', email_body=None, attachment=None, attachment_name=None):
+    """
+    sends an email (using exchangelib)
+
+    :param credentials: (dict) with keys email_address and email_password
+    :param email_list: (list) of email addresses that will receive the email
+    :param email_subject: (str) subject line
+    :param email_body: (str, HTMLBody) body of email
+    :param attachment: (str, bytes, io.BytesIO) path or bytes of attachment
+    :param attachment_name: (str) name of attachment, use when attachment is bytes or io.BytesIO
+    :return:
+    """
     if isinstance(email_list, list):
         to_list = []
         for each in email_list:
@@ -148,7 +221,7 @@ def send_email(credentials, email_list=None, email_subject='', email_body=None, 
             attachment = attachment
         elif isinstance(attachment, BytesIO().__class__):
             attachment = attachment.read()
-        else:
+        elif attachment:
             raise SendError(
                 f'attachment must be the path to a file or {BytesIO().__class__} or {bytes}. Cannot be {type(attachment)}.')
         if attachment and attachment_name:
